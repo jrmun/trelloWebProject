0da44a8a54795f12a248d7c00fbfb42c6264f144
Lee <jrmun123@gmail.com>
2023-08-07 19:57:51 +0900

    add: public

--- app.js
+++ app.js
@@ -5,6 +5,8 @@ const app = express();
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+app.use(express.static('public'));
+
 fs.readdirSync('./routes').forEach((routes) => {
     app.use('/', require(`./routes/${routes}`));
 });
